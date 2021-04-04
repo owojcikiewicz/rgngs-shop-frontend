b37c4feee3c94bda3ed5c80f431f090322070b00
Oskar <saturdaysheroes.business@gmail.com>
2021-04-04 18:37:44 +0200

    added custom parsing for VIP

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -16,16 +16,30 @@ class Item extends React.Component {
     };
 
     render() {  
-        return (
-            <div class="item-layout">
-                <div class="item-main">
-                    <div class="item-header" style={{backgroundImage:this.gradient}}><label class="item-title">{this.title}</label></div>
-                    <button class="item-button" style={{backgroundImage:this.gradient}} onClick={this.buy}>KUP</button>
-                    <div class="item-price-div"><label class="item-price">{this.price}</label></div>
-                    <div class="item-content">{this.description}</div>
+        if (this.title === "VIP") {
+            let str = "Wyróźnij się z tłumu i zyskaj eksluzywne benefity!\n\n- Możliwość stworzenia dwóch postaci\n- Ekskluzywne pojazdy VIP\n- Własne tablice rejestracyjne\n- Pełna oferta Paralake Customs\n- 50+ miejsca w banku\n- 50+ miejsca w sejfach\n- Boost do regeneracji kondycji\n- 2 dodatkowe doniczki\n- 2 dodatkowe lampy\n- 2 dodatkowe wentylatory\n\nI wiele, wiele więcej!";
+            return (
+                <div class="item-layout">
+                    <div class="item-main">
+                        <div class="item-header" style={{backgroundImage:this.gradient}}><label class="item-title">{this.title}</label></div>
+                        <button class="item-button" style={{backgroundImage:this.gradient}} onClick={this.buy}>KUP</button>
+                        <div class="item-price-div"><label class="item-price">{this.price}</label></div>
+                        <div class="item-content">{<p>{str}</p>}</div>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return (
+                <div class="item-layout">
+                    <div class="item-main">
+                        <div class="item-header" style={{backgroundImage:this.gradient}}><label class="item-title">{this.title}</label></div>
+                        <button class="item-button" style={{backgroundImage:this.gradient}} onClick={this.buy}>KUP</button>
+                        <div class="item-price-div"><label class="item-price">{this.price}</label></div>
+                        <div class="item-content">{this.description}</div>
+                    </div>
+                </div>
+            )
+        };
     };
 }
 
