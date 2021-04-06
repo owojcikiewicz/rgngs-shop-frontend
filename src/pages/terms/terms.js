414c33a2b83ed2f9f90f89d3b21d1efecdae5bc3
Oskar <saturdaysheroes.business@gmail.com>
2021-04-06 13:35:07 +0200

    update terms

--- src/pages/terms/terms.js
+++ src/pages/terms/terms.js
@@ -5,7 +5,7 @@ class Terms extends React.Component {
   render() {
   return (
     <div class="terms-main">
-      <h1 class="terms-main-text">REGULAMIN</h1>
+      <h1 class="terms-main-text">REGULAMIN SKLEPU</h1>
         <h2>I. Something</h2>
         <ul>
           <li>um is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry"s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets c</li>
