8b6fd03ceef92b7683657a8733f16ac4a156cc7b
Soberaja <52428868+Soberaja@users.noreply.github.com>
2021-04-07 15:24:29 +0200

    added profile menu and icons

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,6 +2,9 @@ import React from "react";
 import axios from "axios";
 import "./header.css"
 import Steam from "../../assets/steam.png";
+import Help from "../../assets/lifebuoy.png";
+import Logout from "../../assets/logout.png";
+import History from "../../assets/list.png";
 import Logo from "../../assets/logo.png";
 import {Link} from "react-router-dom";
 import Avatar from "react-avatar";
@@ -32,24 +35,24 @@ class Header extends React.Component {
           <div class="profile-info">
             <Avatar className="profile-avatar" size="40" src={this.props.info.avatar.medium} round={true} alt={"https://steamuserimages-a.akamaihd.net/ugc/885384897182110030/F095539864AC9E94AE5236E04C8CA7C2725BCEFF/"} />
             <label class="profile-name">{this.props.info.name}</label>
-            <label class="profile-rcoins">rCoins:{this.props.user.rcoins}</label>
+            <label class="profile-rcoins">rCoins: {this.props.user.rcoins}</label>
           </div>
           
           <div class="button-container">
-            <button class="history-btn">
-              <img class="history-img" src={Steam} alt="Steam"></img>
+            <a class="history-btn" href="#" rel="noreferrer" >
+              <img class="history-img" src={History} alt="Steam"></img>
               <label class="history-text">HISTORIA ZAMÓWIEŃ</label>
-            </button>
+            </a>
             
-            <button class="help-btn">
-              <img class="help-img" src={Steam} alt="Steam"></img>
+            <a class="help-btn" href="#" rel="noreferrer" >
+              <img class="help-img" src={Help} alt="Steam"></img>
               <label class="help-text">CENTRUM POMOCY</label>
-            </button>
+            </a>
             
-            <button class="logout-btn">
-              <img class="logout-img" src={Steam} alt="Steam"></img>
+            <a class="logout-btn" href="#" rel="noreferrer" >
+              <img class="logout-img" src={Logout} alt="Steam"></img>
               <label class="logout-text">WYLOGUJ SIĘ</label>
-            </button>
+            </a>
          
           </div>
         </div>
