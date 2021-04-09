0cf9fa63ce35ae64284c67b4bb82a28f9f69453b
Oskar <saturdaysheroes.business@gmail.com>
2021-04-09 16:42:18 +0200

    cleanups

--- src/pages/page404/page404.js
+++ src/pages/page404/page404.js
@@ -3,12 +3,12 @@ import "./page404.css"
 
 class Page404 extends React.Component {
   render() {
-  return (
-    <div class="page404-main">
-      <label class="page404-main-text">404</label>
-      <label class="page404-second-text">Przepraszam ale nie możemy znaleźć tej strony</label>
-    </div>
-  );
+    return (
+      <div class="page404-main">
+        <label class="page404-main-text">404</label>
+        <label class="page404-second-text">Przepraszam ale nie możemy znaleźć tej strony</label>
+      </div>
+    );
   }
 }
 
