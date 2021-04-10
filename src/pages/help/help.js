d7b9828e8dd2f4107e9cf13ec5291e499e83cb84
Soberaja <52428868+Soberaja@users.noreply.github.com>
2021-04-10 14:28:31 +0200

    added help page

--- src/pages/help/help.js
+++ src/pages/help/help.js
@@ -1,27 +1,68 @@
 import React from "react";
 import {Redirect} from "react-router-dom";
+import "./help.css";
 
 class Help extends React.Component {
     constructor(props) {
-        super(props);
+      super(props);
+      this.state = {  
+        order:"",
+      };
+      
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     };
-
+  
+    handleChange(event) {
+      const target = event.target;
+      const value = target.type === "checkbox" ? target.checked : target.value;
+      const name = target.name;
+      this.setState({
+        [name]: value
+      });
+    }
+  
     // @TODO: Post to API. 
-    handleSubmit() {
+       handleSubmit() {
 
     };
 
     // @TODO: Render help form. 
     // this.props.orders.
+  
     render() {
         if (!this.props.login || this.props.login.loggedin !== true || !this.props.login.user) {
             return <Redirect to="/"/>
         };
+      return (
+        <div class="help-container">
+         <h1 class="help-main-text">CENTRUM POMOCY</h1>
+         <form class="help-form" autoComplete="off" onSubmit={this.handleSubmit}>
 
-        return (
-           <label>hi</label> 
-        );
-    };
-};
+          <div class="help-orderSelect">
+            <label>Wybierz zamówienie</label>
+            <select class="orderSelect" name="order" value={this.state.order} onChange={this.handleChange}> 
+                {this.props.orders.map((val, key) =>{
+                return (
+                    <option value={val.package}>{val.package}</option>
+                    );
+                 })}
+            </select> 
+            </div>
 
-export default Help;
+            <div class="help-text-container">
+                <label class="help-label">Opisz swój problem</label>
+                <input class="help-space" name="name" type="text" placeholder="Opisz swój problem." value={this.state.nameInput} onChange={this.handleChange}/>
+            </div>
+
+            <div class="help-accept-div">
+                <input class="help-accept" type="submit" value="Submit"></input>
+            </div>
+         </form>
+        </div>
+      );
+    }
+  }
+  
+  export default Help;
+  
